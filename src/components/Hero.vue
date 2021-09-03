5135a911b1c704f10fc715b839cbe1d4bfa88e64
ellenbechtel <ellen.bechtel@gmail.com>
2021-09-03 13:04:54 -0400

    clean d3 from hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -56,8 +56,6 @@
         data() {
           return {
             publicPath: process.env.BASE_URL, // allows app to find the files when on different deployment roots
-            d3: null,
-            viz_list: null,
           }
         },
         mounted(){
@@ -65,24 +63,7 @@
           this.loadData();  
         },
         methods:{
-          loadData() {
-            const self = this;
-
-            // read in data 
-            let promises = [
-            self.d3.csv(self.publicPath + "viz-list.csv",  this.d3.autotype) // list of published viz from drupal page
-            ];
-            Promise.all(promises).then(self.callback); // once it's loaded
-          },
-          callback(data) {
-            // assign data
-            this.viz_list = data[0];
-            // console.log(this.viz_list)
-          },
-          goTo(data){
-            // use data to dynamically load url to button so it functions like a link
-
-          }
+          
         }
     }
 </script>
