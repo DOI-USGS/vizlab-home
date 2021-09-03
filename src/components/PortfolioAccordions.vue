3f4db51398e0cc8b9a83d2ad66067c62c9c69229
ellenbechtel <ellen.bechtel@gmail.com>
2021-09-03 13:51:06 -0400

    make card component, pass in props

--- src/components/PortfolioAccordions.vue
+++ src/components/PortfolioAccordions.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="portfolio-accordions">
-    <h3 class="site-preview-heading">Visualizations</h3>
+    <h3 class="site-preview-heading">
+      Visualizations
+    </h3>
 
     <div
       class="usa-accordion"
@@ -13,19 +15,26 @@
           aria-expanded="true"
           aria-controls="m-a1"
         >
-          Basins
+          Integrated Water Science Basins
         </button>
       </h4>
       <div
         id="m-a1"
         class="usa-accordion__content usa-prose"
       >
-        <p>
-          Congress shall make no law respecting an establishment of religion, or
-          prohibiting the free exercise thereof; or abridging the freedom of speech,
-          or of the press; or the right of the people peaceably to assemble, and to
-          petition the Government for a redress of grievances.
-        </p>
+       <div class="card-container grid-container">
+          <ul 
+            class="usa-card-group grid-row"
+          >
+            <PortfolioCard
+        
+              v-for="viz in vizList1"
+              :key="viz.title"
+              :viz="viz"
+              class="tablet:grid-col-4 usa-card"
+            />
+          </ul>
+        </div>
       </div>
 
       <!-- Use the accurate heading level to maintain the document outline -->
@@ -35,21 +44,26 @@
           aria-expanded="false"
           aria-controls="m-a2"
         >
-          Hurricanes
+          River Conditions
         </button>
       </h4>
       <div
         id="m-a2"
         class="usa-accordion__content"
       >
-        <p>
-          A well regulated Militia, being necessary to the security of a free State,
-          the right of the people to keep and bear Arms, shall not be infringed.
-        </p>
-        <ul>
-          <li>This is a list item</li>
-          <li>Another list item</li>
-        </ul>
+        <div class="card-container grid-container">
+          <ul 
+            class="usa-card-group grid-row"
+          >
+            <PortfolioCard
+        
+              v-for="viz in vizList2"
+              :key="viz.title"
+              :viz="viz"
+              class="tablet:grid-col-4 usa-card"
+            />
+          </ul>
+        </div>
       </div>
 
       <!-- Use the accurate heading level to maintain the document outline -->
@@ -59,18 +73,26 @@
           aria-expanded="false"
           aria-controls="m-a3"
         >
-          River Conditions
+          Hurricanes
         </button>
       </h4>
       <div
         id="m-a3"
         class="usa-accordion__content"
       >
-        <p>
-          No Soldier shall, in time of peace be quartered in any house, without the
-          consent of the Owner, nor in time of war, but in a manner to be prescribed
-          by law.
-        </p>
+       <div class="card-container grid-container">
+          <ul 
+            class="usa-card-group grid-row"
+          >
+            <PortfolioCard
+        
+              v-for="viz in vizList3"
+              :key="viz.title"
+              :viz="viz"
+              class="tablet:grid-col-4 usa-card"
+            />
+          </ul>
+        </div>
       </div>
 
       <!-- Use the accurate heading level to maintain the document outline -->
@@ -87,7 +109,19 @@
         id="m-a4"
         class="usa-accordion__content"
       >
-       <PortfolioCards />
+        <div class="card-container grid-container">
+          <ul 
+            class="usa-card-group grid-row"
+          >
+            <PortfolioCard
+        
+              v-for="viz in vizList"
+              :key="viz.title"
+              :viz="viz"
+              class="tablet:grid-col-4 usa-card"
+            />
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -97,14 +131,14 @@
 <script>
   import * as d3Base from 'd3';
   import uswds from 'uswds';
-  import PortfolioCards from './PortfolioCards.vue';
+  import PortfolioCard from './PortfolioCard.vue';
    
   // make sure that the prop for the viz cards is passed in, not sure if this needs to be imported or piped
    
    export default {
         name: 'PortfolioAccordions',
         components: {
-            PortfolioCards
+            PortfolioCard
         },
         props: {
             title: {
@@ -116,8 +150,11 @@
           return {
             publicPath: process.env.BASE_URL, // allows app to find the files when on different deployment roots
             d3: null,
-            viz_list: null,
-            viz_groups: null,
+            vizList: null, // raw list of visualizations
+            vizList1: null, // featured group #1
+            vizList2: null,// featured group #2
+            vizList3: null, // featured group #3
+            vizGroups: ["iws-basin", "river-conditions", "hurricane"]  // these will be the three featured groups of vizzies, in order, pulled from the "groups" property of each object
           }
         },
         mounted(){
@@ -136,8 +173,16 @@
           },
           callback(data) {
             // assign data
-            this.viz_list = data[0];
-            console.log(this.viz_list)
+            this.vizList = data[0];
+            console.log(this.vizList);
+
+            // create groups
+            this.vizList1 = this.vizList.filter((viz) => viz.group === this.vizGroups[0]);
+            this.vizList2 = this.vizList.filter((viz) => viz.group === this.vizGroups[1]);
+            this.vizList3 = this.vizList.filter((viz) => viz.group === this.vizGroups[2]);
+
+
+            
             
             
           }
