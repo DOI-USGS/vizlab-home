6307ae3dc4a8acb8b7936bc3d061093e69027add
collnell <cnell@usgs.gov>
2021-10-05 19:21:57 -0500

    scenter scrollto navigation

--- src/components/ContentHeader.vue
+++ src/components/ContentHeader.vue
@@ -1,30 +1,33 @@
 <template>
   <div>
-<header class="usa-header usa-header--basic">
-  <div class="usa-nav-container">
-    <nav aria-label="Primary navigation" class="usa-nav">
-      <ul class="usa-nav__primary usa-accordion">
-         <li class="usa-nav__primary-item">
-          <button
-            class="usa-accordion__button usa-nav__link"
-            aria-expanded="false"
-            aria-controls="basic-nav-new"
-            onClick="document.getElementById('viz-new').scrollIntoView();"
-          >
-            <span>What's new</span>
-          </button>
-        </li>
-        <li class="usa-nav__primary-item">
-          <button
-            class="usa-nav__button usa-nav__link usa-current"
-            aria-expanded="false"
-            aria-controls="basic-nav-viz"
-            onClick="document.getElementById('viz-cards').scrollIntoView();"
-          >
-            <span>Visualizations</span>
-          </button>
+    <header class="usa-header usa-header--basic">
+      <div class="usa-nav-container">
+        <nav
+          aria-label="Primary navigation"
+          class="usa-nav"
+        >
+          <ul class="usa-nav__primary usa-accordion">
+            <li class="usa-nav__primary-item">
+              <button
+                class="usa-nav__button usa-nav__link"
+                aria-expanded="false"
+                aria-controls="basic-nav-latest"
+                onClick="document.getElementById('viz-menu').scrollIntoView();"
+              >
+                <span>What's new</span>
+              </button>
+            </li>
+            <li class="usa-nav__primary-item">
+              <button
+                class="usa-nav__button usa-nav__link"
+                aria-expanded="false"
+                aria-controls="basic-nav-viz"
+                onClick="document.getElementById('viz-cards').scrollIntoView();"
+              >
+                <span>Visualizations</span>
+              </button>
 
-    <!--       <ul id="basic-nav-section-one" class="usa-nav__submenu">
+              <!--       <ul id="basic-nav-section-one" class="usa-nav__submenu">
             <li class="usa-nav__submenu-item">
               <a href="#">Interactives</a>
             </li>
@@ -35,31 +38,31 @@
               <a href="#">Hurricanes</a>
             </li>
           </ul> -->
-        </li>
-        <li class="usa-nav__primary-item">
-          <button
-            class="usa-nav__button usa-nav__link"
-            aria-expanded="false"
-            aria-controls="basic-nav-about"
-            onClick="document.getElementById('viz-about').scrollIntoView();"
-          >
-            <span>About</span>
-          </button>
-        </li>
-         <li class="usa-nav__primary-item">
-          <button
-            class="usa-nav__button usa-nav__link"
-            aria-expanded="false"
-            aria-controls="basic-nav-follow"
-            onClick="document.getElementById('viz-follow').scrollIntoView();"
-          >
-            <span>Follow us</span>
-          </button>
-        </li>
-      </ul>
-    </nav>
-  </div>
-</header>
+            </li>
+            <li class="usa-nav__primary-item">
+              <button
+                class="usa-nav__button usa-nav__link"
+                aria-expanded="false"
+                aria-controls="basic-nav-about"
+                onClick="document.getElementById('viz-about').scrollIntoView();"
+              >
+                <span>About</span>
+              </button>
+            </li>
+            <li class="usa-nav__primary-item">
+              <button
+                class="usa-nav__button usa-nav__link"
+                aria-expanded="false"
+                aria-controls="basic-nav-follow"
+                onClick="document.getElementById('viz-follow').scrollIntoView();"
+              >
+                <span>Follow us</span>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   </div>
 </template>
 
@@ -84,9 +87,11 @@ $color_menu: #1F7564;
   width: 90%;
   background-color: $color_menu;
 }
-.usa_accordion__button .usa-nav__link {
+.usa_nav__button .usa-nav__link {
   color: white;
   stroke: white;
+      outline: none;
+    outline-offset: 0;
 
 }
   @media (min-width:1024px) {
@@ -101,4 +106,9 @@ button {
   }
 
 }
+/* .usa-nav__primary button:focus {
+    outline: none;
+    outline-offset: 0;
+} */
+
 </style>
