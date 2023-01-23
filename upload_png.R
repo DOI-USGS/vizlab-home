f2880b5db8ea3769324e0b7b6c911f231e0f61e2
Corson-Dosch <hcorson-dosch@usgs.gov>
2023-01-23 17:41:08 -0600

    add comment w/ sign in instructions

--- upload_png.R
+++ upload_png.R
@@ -1,6 +1,7 @@
-# place file in either visualizations/charts or visualizations/thumbnails
+# 1) log in to gs-chs-wma-prod on saml2aws with adfs-wma-developer role 
+# 2) place file in either visualizations/charts or visualizations/thumbnails
 # according to where it needs to be placed on s3
-local_file <- 'visualizations/charts/flow-cartogram-202212.png'
+local_file <- 'visualizations/thumbnails/water_cycle_blog_thumbnail.png'
 s3_file <- local_file
 config_file <- 'lib/cfg/s3_config_viz.yml'
 s3_config <- yaml::yaml.load_file(config_file)
