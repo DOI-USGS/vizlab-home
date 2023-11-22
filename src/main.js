f9f43ab54d1632bb1e0edb497b906e066b59a833
Corson-Dosch <hcorson-dosch@usgs.gov>
2023-11-22 12:36:49 -0600

    replace footer w/ latest USGS Viz ID footer

--- src/main.js
+++ src/main.js
@@ -9,8 +9,10 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
 import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faFlickr } from "@fortawesome/free-brands-svg-icons";
+import { faYoutubeSquare } from "@fortawesome/free-brands-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
-library.add(faTwitterSquare, faFacebookSquare, faGithub, faInstagram);
+library.add(faTwitterSquare, faFacebookSquare, faGithub, faFlickr, faYoutubeSquare, faInstagram);
 
 import App from './App.vue'
 import router from './router'
