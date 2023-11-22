eae3a27ddeec990e658ce3192c7059962e65b45c
Corson-Dosch <hcorson-dosch@usgs.gov>
2023-11-22 12:45:57 -0600

    add latest USGS Viz ID USGS header

--- src/components/HeaderUSGS.vue
+++ src/components/HeaderUSGS.vue
@@ -1,55 +1,28 @@
 <template>
-  <header
-    id="usgs-banner"
-    role="banner"
-  >
-    <div class="logo-container">
-      <a
-        class="logo-header"
-        href="https://www.usgs.gov/mission-areas/water-resources/science/water-data-visualizations"
-        aria-label="Water data visualizations homepage on USGS.gov"
-        target="_blank"
-      >
-        <img
-          class="img"
-          src="@/assets/usgsHeaderAndFooter/images/usgsLogo_white-01.png"
-          alt="USGS logo. Science for a changing world."
-        >
-      </a>
-    </div>
+  <header id="navbar" class="header-nav"  role="banner">
+    <div class="tmp-container">
+        <!-- primary navigation bar -->
+        <!-- search bar-->
+        <div class="header-search">
+        <a class="logo-header" href="https://www.usgs.gov/" target="_blank" title="USGS.gov homepage" aria-label="USGS.gov homepage">
+            <img class="img"  src="@/assets/usgsHeaderAndFooter/images/usgsLogo_white-01.png"  alt="USGS logo. Science for a changing world." target="_blank"/>
+          </a>
+          <form action="https://www.usgs.gov/science-explorer-results" method="GET" id="search-box">
+          <div class="fa-wrapper"><label for="se_search" class="only">Search</label>
+            <input id="se_search" type="search" name="es" placeholder="Search">
+            <button class="fa fa-search" type="submit">
+              <span class="only">Search</span>
+        </button></div>
+          </form>
+        </div>
+        <!-- end search bar-->
+    </div> 
+    <!-- end header-container-->
   </header>
 </template>
 
 <script>
-    export default {
-        name: 'HeaderUSGS'
-    }
 </script>
 
 <style scoped lang="scss">
-  #usgs-banner {
-    display: block;
-    background: #151515;
-  }
-  .logo-container {
-    margin-right: auto;
-    margin-left: auto;
-    padding-left: 15px;
-    padding-right: 15px;
-    border-bottom: 0px solid white;
-  }
-  .logo-container a {
-    text-decoration: none;
-    background: transparent;
-  }
-  .logo-container:after {
-    clear: both;
-  }
-  .logo-header img {
-    margin-top: 10px;
-    margin-bottom: 8px;
-    border: 0;
-    max-width: 100%;
-    height: 65px;
-  }
 </style>
