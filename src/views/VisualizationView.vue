1b540650a685d522fd34deb0d278810d03bdda42
Corson-Dosch <hcorson-dosch@usgs.gov>
2023-11-28 17:23:36 -0600

    add example of d3 data-driven bar chart that updates w/ dropdown

--- src/views/VisualizationView.vue
+++ src/views/VisualizationView.vue
@@ -2,11 +2,14 @@
   <section>
     <h1 class="section-header">svg import w/ d3-added interaction + v-for</h1>
     <RegionalViolins />
+    <h1 class="section-header">d3 bar chart from data</h1>
+    <BarChartExample />
   </section>
 </template>
 
 <script setup>
 import RegionalViolins from '.././components/RegionalViolins.vue';
+import BarChartExample from '.././components/BarChartExample.vue';
 </script>
 
 <style scoped>
