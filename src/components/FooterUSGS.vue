30e0a0e3cfccf275f156d44892e9d922c824de6e
Cee Nell <cnell@usgs.gov>
2024-03-24 11:36:19 -0700

    Update FooterUSGS.vue
    Update accessibility link

--- src/components/FooterUSGS.vue
+++ src/components/FooterUSGS.vue
@@ -14,7 +14,7 @@
               <a href="https://www.usgs.gov/laws/policies_notices.html">Legal</a>
             </li>
             <li class="leaf menu-links menu-level-1">
-              <a href="https://www2.usgs.gov/laws/accessibility.html">Accessibility</a>
+              <a href="https://www.usgs.gov/accessibility">Accessibility</a>
             </li>
             <li class="leaf menu-links menu-level-1">
               <a href="https://www.usgs.gov/sitemap.html">Site Map</a>
