cf59e7d87343d9f08205211b2ec961351c0d14f7
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-04-15 11:52:42 -0500

    Clean up social icon import

--- src/main.js
+++ src/main.js
@@ -7,12 +7,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 // social icons
-import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
-import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faFlickr } from "@fortawesome/free-brands-svg-icons";
-import { faYoutubeSquare } from "@fortawesome/free-brands-svg-icons";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faTwitterSquare, faFacebookSquare, faGithub, faFlickr, faYoutubeSquare, faInstagram } from "@fortawesome/free-brands-svg-icons";
 library.add(faTwitterSquare, faFacebookSquare, faGithub, faFlickr, faYoutubeSquare, faInstagram);
 
 import App from './App.vue'
