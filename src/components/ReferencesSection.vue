c7427a37b2c31f84068711b1750cd7b69f263ba3
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-04-19 11:37:12 -0500

    provide better reference examples and format correctly

--- src/components/ReferencesSection.vue
+++ src/components/ReferencesSection.vue
@@ -8,10 +8,17 @@
           :key="reference.num"
         >
           <p>
-            <span v-html="reference.num" />. <span v-html="reference.authors" /> (<span v-html="reference.year" />) <a
+            <span v-html="reference.num" />. <span v-html="reference.authors" /> (<span v-html="reference.year" />). <a
               :href="reference.link"
               target="_blank"
-            ><span v-html="reference.title" /></a>. <span v-html="reference.ref" />
+            ><span v-html="reference.title" /></a>
+            <span v-if="reference.data_release">: U.S. Geological Survey data release</span>.
+            <span v-if="reference.journal">
+              <span v-html="reference.journal_name" class="journal-name"></span>
+              <span v-if="reference.journal_issue">, {{ reference.journal_issue }}</span>.
+            </span>
+            <span v-if="reference.doi" v-html="reference.doi"></span>
+            <span v-else v-html="reference.link"></span>
           </p>
         </div>
       </div>
@@ -24,4 +31,7 @@
 </script>
 
 <style scoped lang="scss">
+  .journal-name {
+    font-style: italic;
+  }
 </style>
