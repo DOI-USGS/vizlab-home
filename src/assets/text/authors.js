038e0c9dfcb731cebeabf8a5941d997452764352
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-04-22 09:08:30 -0500

    Add all vizlab members to authors.js

--- src/assets/text/authors.js
+++ src/assets/text/authors.js
@@ -23,7 +23,70 @@ export default {
         initials: 'AAA',
         profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer',
         role: 'contributor',
-        contribution: 'contributed to website development and reviewed code'
+        contribution: 'contributed to website development, created data visualizations and reviewed code'
+      },
+      {
+        firstName: 'Elmera',
+        lastName: 'Azadpour',
+        fullName: 'Elmera Azadpour',
+        initials: 'EA',
+        profile_link: 'https://www.usgs.gov/staff-profiles/elmera-azadpour',
+        role: 'contributor',
+        contribution: 'created data visualizations'
+      },
+      {
+        firstName: 'Mandie',
+        lastName: 'Carr',
+        fullName: 'Mandie Carr',
+        initials: 'MC',
+        profile_link: 'https://www.usgs.gov/staff-profiles/amanda-carr',
+        role: 'contributor',
+        contribution: 'wrote the narrative for the site'
+      },
+      {
+        firstName: 'Aileen',
+        lastName: 'Clarke',
+        fullName: 'Aileen Clarke',
+        initials: 'AC',
+        profile_link: 'https://www.usgs.gov/staff-profiles/aileen-m-clarke',
+        role: 'contributor',
+        contribution: 'contributed to website development'
+      },
+      {
+        firstName: 'Leo',
+        lastName: 'Ivey',
+        fullName: 'Leo Ivey',
+        initials: 'LI',
+        profile_link: null,
+        role: 'contributor',
+        contribution: 'created data visualizations'
+      },
+      {
+        firstName: 'Maggie',
+        lastName: 'Jaenicke',
+        fullName: 'Maggie Jaenicke',
+        initials: 'MJ',
+        profile_link: 'https://www.usgs.gov/staff-profiles/margaret-maggie-jaenicke',
+        role: 'contributor',
+        contribution: 'contributed to website development'
+      },
+      {
+        firstName: 'Jeffrey',
+        lastName: 'Kwang',
+        fullName: 'Jeffrey Kwang',
+        initials: 'JK',
+        profile_link: 'https://www.usgs.gov/staff-profiles/jeffrey-kwang',
+        role: 'contributor',
+        contribution: 'created data visualizations'
+      },
+      {
+        firstName: 'Anthony',
+        lastName: 'Martinez',
+        fullName: 'Anthony Martinez',
+        initials: 'AM',
+        profile_link: 'https://www.usgs.gov/staff-profiles/anthony-martinez',
+        role: 'contributor',
+        contribution: 'created data visualizations'
       },
       {
         firstName: 'Cee',
@@ -33,6 +96,15 @@ export default {
         profile_link: 'https://www.usgs.gov/staff-profiles/cee-nell',
         role: 'team lead',
         contribution: 'acted in an advisory role, contributed to website development, and reviewed code'
+      },
+      {
+        firstName: 'Kaysa',
+        lastName: 'Vaarre-Lamoureux',
+        fullName: 'Kaysa Vaarre-Lamoureux',
+        initials: 'KVL',
+        profile_link: null,
+        role: 'contributor',
+        contribution: 'created data visualizations'
       }
     ]
 };
