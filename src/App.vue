3ce17a686e90c156ba8e45205b3a86f5d3f33a29
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-04-25 15:49:26 -0500

    use consisten @ syntax for component import statements

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,13 @@
 <script setup>
   import { onMounted } from "vue";
   import { RouterView } from 'vue-router'
-  import WindowSize from "./components/WindowSize.vue";
-  import HeaderUSWDSBanner from "./components/HeaderUSWDSBanner.vue";
-  import HeaderUSGS from './components/HeaderUSGS.vue';
-  import WorkInProgressWarning from "./components/WorkInProgressWarning.vue";
-  import PreFooterCodeLinks from "./components/PreFooterCodeLinks.vue";
-  import FooterUSGS from './components/FooterUSGS.vue';
-  import { useWindowSizeStore } from './stores/WindowSizeStore';
+  import WindowSize from "@/components/WindowSize.vue";
+  import HeaderUSWDSBanner from "@/components/HeaderUSWDSBanner.vue";
+  import HeaderUSGS from '@/components/HeaderUSGS.vue';
+  import WorkInProgressWarning from "@/components/WorkInProgressWarning.vue";
+  import PreFooterCodeLinks from "@/components/PreFooterCodeLinks.vue";
+  import FooterUSGS from '@/components/FooterUSGS.vue';
+  import { useWindowSizeStore } from '@/stores/WindowSizeStore';
 
   const windowSizeStore = useWindowSizeStore();
   const typeOfEnv = import.meta.env.VITE_APP_TIER;
