4249246caa0d85fdb61a4e8cbdb58ade051b7f18
Azadpour, Elmera <eazadpour@usgs.gov>
2025-11-17 08:28:15 -0800

    drop 2022 blog content and renumber

--- src/assets/content/WhatsNew.js
+++ src/assets/content/WhatsNew.js
@@ -1,7 +1,7 @@
 export default {
     blogListItems: [
         {
-            id: '7',
+            id: '6',
             date: '11/14/2025:',
             title: "Charting 'tidycensus' data with R ",
             url: 'https://waterdata.usgs.gov/blog/tidycensus-viz/',
@@ -9,7 +9,7 @@ export default {
             img_alt: 'Census data viz with the USGS Vizlab. Large text filled with colorful census data visualizations on a dark background.'
         }, 
         {
-            id: '6',
+            id: '5',
             date: '12/09/2024:',
             title: 'Mapping water insecurity in R with tidycensus',
             url: 'https://waterdata.usgs.gov/blog/acs-maps/',
@@ -17,7 +17,7 @@ export default {
             img_alt: 'Mapping water insecurity in R with tidycensus. A map of the western US showing median gross rent in 2022 with highest rent in southern CA and the Bay area.'
         },  
         {
-            id: '5',
+            id: '4',
             date: '07/21/2023:',
             title: 'Jazz up your ggplots!',
             url: 'https://waterdata.usgs.gov/blog/ggplot-jazz/',
@@ -25,28 +25,22 @@ export default {
             img_alt: 'Musical notes drawn with the center of the notes containing data viz example'
         },        
         {
-            id: '4',
+            id: '3',
             date: '05/3/2023:',
             title: 'The 30 Day Chart Challenge with the USGS VizLab',
             url: 'https://waterdata.usgs.gov/blog/chart-challenge-2023/',
         },        
         {
-            id: '3',
+            id: '2',
             date: '01/11/2023:',
             title: 'Origin and development of a Snowflake Map',
             url: 'https://waterdata.usgs.gov/blog/snow-tiles-demo/',
         },        
         {
-            id: '2',
+            id: '1',
             date: '01/04/2023:',
             title: 'A New Take on the Water Cycle',
             url: 'https://waterdata.usgs.gov/blog/water-cycle-release/',
-        },        
-        {
-            id: '1',
-            date: '09/08/2022:',
-            title: 'Water data science in 2022',
-            url: 'https://waterdata.usgs.gov/blog/water-data-science-2022/',
         }
     ]
 };
