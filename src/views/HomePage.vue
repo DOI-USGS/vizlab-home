13c6961e5351819663efeb237090591358a9a45d
Cee <cnell@usgs.gov>
2025-12-28 22:45:20 -0600

    build page layout

---
+++ src/views/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+  <section id="visualization-container">
+    <!-- header -->
+    <StickyNav />
+
+    <!-- main -->
+    <main class="content">
+      <!-- interactive websites -->
+      <VizSection :items="websites" />
+    </main>
+  </section>
+</template>
+
+<script setup>
+import { computed } from "vue"
+import StickyNav from "@/components/StickyNav.vue"
+import VizSection from "@/components/VizSection.vue"
+
+import viz from "@/assets/content/viz-list.json"
+
+const websites = computed(() => viz?.websites || [])
+</script>
